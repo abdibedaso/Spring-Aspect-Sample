14c2788a671bc54f5fb52aa7bb1ffb5e82ed6b57
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-31 00:51:44 -0500

    init spring boot proj

---
+++ src/test/java/com/geekcolab/development/SpringAspectSample/SpringAspectSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekcolab.development.SpringAspectSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAspectSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
