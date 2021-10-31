57ae22e37ecefeb18b21fcd486c45c79354192f7
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-31 01:32:54 -0500

    imp before advice

--- src/main/java/com/geekcolab/development/SpringAspectSample/aspect/LoggingAspect.java
+++ src/main/java/com/geekcolab/development/SpringAspectSample/aspect/LoggingAspect.java
@@ -1,14 +1,41 @@
 package com.geekcolab.development.SpringAspectSample.aspect;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
 
 @Component
 @Aspect
 public class LoggingAspect {
 
+    private static final Logger LOG = LoggerFactory.getLogger(LoggingAspect.class);
+
     @Pointcut("@annotation(Loggable)")
     public void executeLogging(){}
+
+    @Before("executeLogging()")
+    public void logBeforeMethodCall(JoinPoint joinPoint){
+        LOG.info("-------------------------------------------");
+        LOG.info("Logging - with Before Advice");
+        StringBuilder message = new StringBuilder("Method: ");
+        message.append(joinPoint.getSignature().getName());
+        Object[] args = joinPoint.getArgs();
+        if(null!=args && args.length>0){
+            message.append(" args=[ | ");
+            Arrays.asList(args).forEach(arg->{
+                message.append(arg).append(" | ");
+            });
+            message.append("]");
+        }
+        LOG.info(message.toString());
+        LOG.info("-------------------------------------------");
+    }
 
 }
