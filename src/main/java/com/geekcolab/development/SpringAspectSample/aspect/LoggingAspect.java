70297f4efb4757b608acccb19ec1f29fbb42c647
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-31 00:58:41 -0500

    create logging aspect

---
+++ src/main/java/com/geekcolab/development/SpringAspectSample/aspect/LoggingAspect.java
@@ -0,0 +1,14 @@
+package com.geekcolab.development.SpringAspectSample.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    @Pointcut("@annotation(Loggable)")
+    public void executeLogging(){}
+
+}
